dbbac258dffeba42e07b0a567f9eb3b5e5f6e43a
andres felipe ospina <afelipe.ospina@gmail.com>
2024-10-18 23:41:08 -0500

    MainWindow

--- WPFApp/MainWindow.xaml.cs
+++ WPFApp/MainWindow.xaml.cs
@@ -24,12 +24,40 @@ namespace WPFApp
         void CreateTask()
         {
             Task T;
-            
+            Action Code = new Action(ShowMessage);
+            T = new Task(Code);
+
+            Task T2 = new Task(delegate
+            {
+                MessageBox.Show("Ejecutando una tarea en un metodo anonimo ");
+            }
+            );
+            Task T3A = new Task(() => ShowMessage());
+            Task T3 = new Task(
+                () => ShowMessage());
+
+            Task T4 = new Task(() => MessageBox.Show("Ejecutando la tarea 4"));
+
+            Task T5 = new Task(() =>
+                {
+                    DateTime CurrenDate = DateTime.Today;
+                    DateTime StarDate= CurrenDate.AddDays(30);
+                    MessageBox.Show($"Tarea 5.  Fecha calculada: {StarDate}");
+                }
+                );
+
+            Task T6 = new Task((message) =>
+            MessageBox.Show(message.ToString()), "exprecion lambda con parametros");
+
         }
+
+        
 
         void ShowMessage()
         {
             MessageBox.Show("Ejecutando el metodo ShowMessage");
         }
-    }
+
+        
+}
 }
